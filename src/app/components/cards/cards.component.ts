60aa47bf3f34aa771aaebdd9b43cc46967da63e5
Julian Rodriguez <jrodriguezalmanza98@gmail.com>
2021-02-10 12:56:17 -0500

    componente cards y card creados

--- src/app/components/cards/cards.component.ts
+++ src/app/components/cards/cards.component.ts
@@ -6,7 +6,23 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./cards.component.scss']
 })
 export class CardsComponent implements OnInit {
-
+  cards:any[]=[
+    {
+      title : "Brand Recognition",
+      text : "Boost your brand recognition with each click. Generic links don't mean a thing.Branded links help instil confidence in your content", 
+      img: "../../../assets/images/icon-brand-recognition.svg"
+    },
+    {
+      title:"Detailed Records",
+      text: "Gain insights into who is clicking your links. Knowing when and where people engage with your content helps inform better decisions.",
+      img: "../../../assets/images/icon-detailed-records.svg"
+    },
+    {
+      title:"Fully Customizable",
+      text:"Improve brand awareness and content discoverability trough customizable links, supercharging audience engagemnt.",
+      img:"../../../assets/images/icon-fully-customizable.svg"
+    }
+  ];
   constructor() { }
 
   ngOnInit(): void {
